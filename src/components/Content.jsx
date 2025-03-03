fb195a1749b0b01aa690bb3969778a9b66e77f55
e.khisamutdinov <e.khisamutdinov@ngrsoftlab.ru>
2025-03-03 10:33:32 +0300

    см

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -3,16 +3,25 @@ import '../styles/Content.css';
 
 const Content = ({ risks, onShowRiskForm }) => {
   return (
-    <div className="content">
+    <div className="content-grid"> {/* Изменили класс для грида */}
       {risks.map((risk, index) => (
-        <div key={index} className="card">
-          <div className="card-header">
-            <span className="card-date">{risk.description}</span>
-            <span className="card-status">{risk.status}</span>
+        <div key={index} className="card-grid"> {/* Изменили класс для карточки */}
+          <div className="card-header-grid"> {/* Верхняя часть карточки */}
+            <img src={risk.photo} alt="risk" className="risk-photo" />
+            <div className="risk-info">
+              <span className="card-critical">{risk.critical}</span>
+              <span className="card-date">{risk.date}</span>
+              <span className="card-number">Риск {risk.number}</span>
+            </div>
           </div>
-          <h3>{risk.type}</h3>
-          <p>{risk.type}</p>
-          {/* <button onClick={onShowRiskForm}>Перейти</button> */}
+          <div className="card-middle-grid"> {/* Средняя часть карточки */}
+            <span className="card-category">{risk.category}</span>
+            <span className="card-sector">{risk.sector}</span>
+            <span className="card-section">{risk.section}</span>
+          </div>
+          <div className="card-description-grid"> {/* Нижняя часть карточки */}
+            <p>{risk.description}</p>
+          </div>
         </div>
       ))}
     </div>
