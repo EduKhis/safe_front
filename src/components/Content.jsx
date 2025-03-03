d981767a45ad244fa5039f70f612c9a433e65435
e.khisamutdinov <e.khisamutdinov@ngrsoftlab.ru>
2025-03-03 15:18:03 +0300

    as

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -7,8 +7,7 @@ const Content = ({ risks, onShowRiskForm }) => {
       {risks.map((risk, index) => (
         <div key={index} className="card-grid"> {/* Изменили класс для карточки */}
           <div className="card-header-grid"> {/* Верхняя часть карточки */}
-            <img src={`/images/${risk.photo}`} alt="risk" className="risk-photo" />
-            console.log(/images/${risk.photo});
+            <img src={risk.photo} alt="risk" className="risk-photo" />
             <div className="risk-info">
               <span className="card-critical">{risk.criticaly}</span>
               <span className="card-date">{risk.dateTimeFix}</span>
