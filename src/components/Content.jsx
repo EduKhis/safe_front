9a25dd2c58eb7465d5c25cc2866c5679536d9c7b
e.khisamutdinov <e.khisamutdinov@ngrsoftlab.ru>
2025-03-05 11:53:02 +0300

    12

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -17,13 +17,15 @@ const Content = ({ risks, onShowRiskForm }) => {
           <div className="card-header-grid">
             <img src={risk.photo} alt="risk" className="risk-photo" />
             <div className="risk-info">
-              <span className={`card-critical ${risk.criticaly?.toLowerCase() || 'gray'}`}></span>
-              <span className="card-number">{risk.type} {risk.id}</span>
+              <span className="card-critical-wrapper">
+                <span className={`card-critical ${risk.criticaly?.toLowerCase() || 'gray'}`}></span>
+                <span className="card-number">{risk.type} {risk.id}</span>
+              </span>
               <span className="card-date">{risk.dateTimeFix}</span>
             </div>
           </div>
+
           <div className="card-middle-grid">
-            <span className="card-category">{risk.category}</span>
             <span className="card-category">{risk.category}</span>
             <span className="card-sector-section">{risk.sector}. {risk.section}</span>
           </div>
